9b933ab77ebbb7aa1559ab3df1f16dc843412eef
Molly S. Peeples <molly@stsci.edu>
2015-09-25 11:24:34 -0400

    runs quick_look from command line

---
+++ test_msp.py
@@ -0,0 +1,23 @@
+from astropy.io import fits
+from astropy.io import ascii 
+import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from matplotlib import colors
+import glob
+import os 
+import sys  
+
+import quick_look 
+
+
+mpl.rcParams['agg.path.chunksize'] = 10000 
+ 
+
+
+quick_look.get_quick_look() 
+
+
+
